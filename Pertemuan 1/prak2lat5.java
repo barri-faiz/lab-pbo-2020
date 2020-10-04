c252c96ba60c169583a8465666f7fc24a4e236fa
Khairul Barri Faiz <khairulbarri.f@gmail.com>
2020-10-04 20:35:27 +0700

    Pertemuan 1

---
+++ Pertemuan 1/prak2lat5.java
@@ -0,0 +1,13 @@
+public class prak2lat5 {
+	public static void main(String[] args) {
+		boolean a = true;
+		boolean b = false;
+
+		System.out.println ("a && b = " +(a&&b));
+		System.out.println ("a || b = " +(a||b));
+		System.out.println ("!(a && b) = " +!(a&&b));
+		System.out.println ("!(a || b) = " +!(a||b));
+	}
+}
+
+// Code diatas bertujuan untuk mencoba operator logika
